b11cd0f4ae525b117dd643750a5f126ac8456a74
Daniel B <busisd@carleton.edu>
2018-09-17 11:54:59 -0500

    missed all the files on my first  commit

---
+++ primecheckertests.py
@@ -0,0 +1,45 @@
+'''
+   primecheckertests.py
+   Jeff Ondich, 9 May 2012
+   Updated for use in a lab exercise, 4 Nov 2013
+'''
+
+import primechecker
+import unittest
+
+class PrimeCheckerTester(unittest.TestCase):
+    def setUp(self):
+        print("setUp")
+        self.prime_checker = primechecker.PrimeChecker(100)
+
+    def tearDown(self):
+        print("tearDown")
+        pass
+
+    def test_zero(self):
+        print("test_zero")
+        self.assertRaises(ValueError, self.prime_checker.is_prime, 0)
+
+    def test_negative(self):
+        print("test_negative")
+        self.assertRaises(ValueError, self.prime_checker.is_prime, -5)
+
+    def test_two(self):
+        print("test_two")
+        self.assertTrue(self.prime_checker.is_prime(2))
+
+    def test_prime(self):
+        print("test_prime")
+        self.assertTrue(self.prime_checker.is_prime(97))
+
+    def test_composite(self):
+        print("test_composite")
+        self.assertFalse(self.prime_checker.is_prime(96))
+
+    def test_primes_below(self):
+        print("test_primes_below")
+        self.assertEqual(self.prime_checker.get_primes_below(20), [2, 3, 5, 7, 11, 13, 17, 19])
+
+if __name__ == '__main__':
+    unittest.main()
+
